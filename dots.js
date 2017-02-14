5a0a5f918d3d766f3238b7f94fead8570dbb6f09
71619997a <marksg07@gmail.com>
2017-02-14 14:41:39 -0500

    Circles thing

--- dots.js
+++ dots.js
@@ -1,8 +1,20 @@
-var canvas = document.getElementById("thecanvas");
+var canvas = document.getElementById("dotcanvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#0000ff";
+ctx.fillStyle = "#ff0000";
+lastcoos = null;
 canvas.addEventListener("click", function(e) {
-    ctx.fillRect(e.pageX - 50, e.pageY - 50, 100, 100);
+    if(lastcoos === null) {
+        lastcoos = [e.pageX, e.pageY];
+    }
+    ctx.beginPath();
+    ctx.moveTo(lastcoos[0], lastcoos[1]);
+    ctx.lineTo(e.pageX, e.pageY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(e.pageX, e.pageY, 25, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    lastcoos = [e.pageX, e.pageY];
 });
                      
 var clearBut = document.getElementById("clear");
