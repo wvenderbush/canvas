19d3c95f749195012a7d282f168c4817e86aa58b
71619997a <gmarks@stuy.edu>
2017-02-13 21:31:39 -0500

    Make drawing squares work

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("thecanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#0000ff";
 canvas.addEventListener("click", function(e) {
-    ctx.fillRect(e.pageX - 50, e.pageY - 50, e.pageX + 50, e.pageY + 50);
+    ctx.fillRect(e.pageX - 50, e.pageY - 50, 100, 100);
 });
                      
   
