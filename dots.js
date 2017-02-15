12b58a3e46005d95007203a0f1935831cb2b7009
71619997a <marksg07@gmail.com>
2017-02-15 14:21:58 -0500

    page -> offset, preventDefault-ing

--- dots.js
+++ dots.js
@@ -3,22 +3,24 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#ff0000";
 lastcoos = null;
 canvas.addEventListener("click", function(e) {
+    e.preventDefault();
     if(lastcoos === null) {
-        lastcoos = [e.pageX, e.pageY];
+        lastcoos = [e.offsetX, e.offsetY];
     }
     ctx.beginPath();
     ctx.moveTo(lastcoos[0], lastcoos[1]);
-    ctx.lineTo(e.pageX, e.pageY);
+    ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     ctx.beginPath();
-    ctx.arc(e.pageX, e.pageY, 25, 0, 2*Math.PI);
+    ctx.arc(e.offsetX, e.offsetY, 25, 0, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
-    lastcoos = [e.pageX, e.pageY];
+    lastcoos = [e.offsetX, e.offsetY];
 });
                      
 var clearBut = document.getElementById("clear");
-clearBut.addEventListener("click", function() {
+clearBut.addEventListener("click", function(e) {
+    e.preventDefault();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     lastcoos = null;
 });
